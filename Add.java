4846670a861cb30d7b85dd8f43cfbceec9ac2f74
chandu817 <68581904+chandu817@users.noreply.github.com>
2022-05-17 11:23:28 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Add
+{
+	public static void main(String args[])
+	{
+		int n = 5, m = 10;
+		int sum = n + m;
+		System.out.println("Sum of the numbers = "+sum);
+	}
+}
